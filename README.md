31921ebba63c5fa39d3b8742f44670d18a96a212
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2024-12-10 23:42:56 +0100

    :technologist: Use direnv and nix flakes to configure development environment

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cad
+
+## Local Development
+
+### NixOS
+
+#### Install Dependencies
+
+##### Using `direnv`
+
+```bash
+direnv allow
+```
+
+### Start OpenSCAD
+
+```bash
+openscad &
+```
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+      devShells.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.openscad
+            ];
+          };
+        };
+    };
+}
